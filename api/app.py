8a24e0c15a380fecabdafd28318668ba1ae84017
PranavSanjayShinde <shindepranav097@gmail.com>
2026-02-10 10:20:14 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -85,7 +85,7 @@ def google_form():
                         if (distance <= maxDistance) {{
                             document.getElementById('form-container').innerHTML = `
                                 <iframe 
-                                    src="https://forms.gle/cY3RNGQHuR5GVNSz9"
+                                    src="https://forms.gle/QmxdKu826wPstyJ77"
                                     width="640" height="520" frameborder="0">
                                     Loading…
                                 </iframe>`;
